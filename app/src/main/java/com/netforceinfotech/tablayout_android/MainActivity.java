101ea0f2e0ee08604aaacd089dc9b3ed4f0ce7a6
tashidev <tenzinchoephel2050@gmail.com>
2016-10-17 11:19:47 +0530

    Initial commit

---
+++ app/src/main/java/com/netforceinfotech/tablayout_android/MainActivity.java
@@ -0,0 +1,32 @@
+package com.netforceinfotech.tablayout_android;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Transition;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//              MyFragment frag= new MyFragment();
+//        SecondFragment secondfrag = new SecondFragment();
+//
+//        FragmentManager manager = getSupportFragmentManager();
+//        FragmentTransaction transaction =manager.beginTransaction();
+//       transaction.add(R.id.activity_main,frag,"My First Fragment");
+//        transaction.add(R.id.tashi,secondfrag,"My First Fragment");
+//       transaction.commit();
+
+
+        
+
+    }
+
+
+    }
+
